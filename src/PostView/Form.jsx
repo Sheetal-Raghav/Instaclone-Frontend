2c0fffa4a3a8d69ea8630e226e02c4cca28facdf
Sheetal Raghav <119871180+Sheetal-Raghav@users.noreply.github.com>
2022-12-17 20:37:42 +0530

    Update Form.jsx

--- src/PostView/Form.jsx
+++ src/PostView/Form.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './Form.css'
  import axios from "axios";
 
-const apiURL = "https://insta-backend-x8sn.onrender.com/post";
+const apiURL = "https://backend-1pvm.onrender.com/post";
 function Form({ setForm, postAdded }) {
     let [formData, setFormData] = useState({ name: '', location: '', description: '' })
     let [file, setFile] = useState(null);
